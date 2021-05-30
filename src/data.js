5f3bc5cbf3e30e0c8af66c60e871c021767ee9ce
Bibek Chaudhary <kingbibekc@gmail.com>
2021-05-30 18:08:36 +0545

    first commit

---
+++ src/data.js
@@ -0,0 +1,45 @@
+const data = [
+    {
+        id: 1,
+        name: 'Kate Johnson',
+        age: 25,
+        image: 'https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjd8fHBlb3BsZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    },
+
+    {
+        id: 2,
+        name: 'David Mason',
+        age: 27,
+        image: 'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cGVvcGxlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    },
+
+    {
+        id: 3,
+        name: 'Smith Richs',
+        age: 21,
+        image: 'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVvcGxlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    },
+
+    {
+        id: 4,
+        name: 'Cameron Shane',
+        age: 19,
+        image: 'https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjh8fHBlb3BsZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    },
+
+    {
+        id: 5,
+        name: 'Emily Cents',
+        age: 20,
+        image: 'https://images.unsplash.com/photo-1524250502761-1ac6f2e30d43?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTd8fHBlb3BsZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    },
+
+    {
+        id: 6,
+        name: 'Rachel Green',
+        age: 23,
+        image: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVvcGxlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    }
+];
+
+export default data;
